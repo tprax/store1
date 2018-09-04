8e7d3e575686465a9a3988c8479afc3707397c60
tprax <tprax01@gmail.com>
2018-09-04 16:50:28 -0600

    got department crud working

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,22 +1,50 @@
 class DepartmentsController < ApplicationController
+  before_action :set_department, only: [:show, :edit, :update, :destroy]
   def index
+    @departments = Department.all
   end
 
   def show
   end
 
   def new
+    @department = Department.new
+    # render partial: "form"
   end
 
   def create
+    @department = Department.new(department_params)
+    if @department.save
+      redirect_to departments_path
+    else
+      render :new
+    end
   end
 
   def edit
+    # render partial: "form"
   end
 
   def update
+    if @department.update(department_params)
+      redirect_to departments_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @department.destroy
+    redirect_to departments_path
   end
+
+  private
+  def set_department
+    @department = Department.find(params[:id])
+  end
+
+  def department_params
+    params.require(:department).permit(:name)
+  end
+
 end
